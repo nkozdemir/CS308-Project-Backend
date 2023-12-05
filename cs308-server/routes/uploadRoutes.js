184c6fd4d77ddfdbc250cfacff0806b929e3ca50
k44n-max <73305630+nkozdemir@users.noreply.github.com>
2023-12-05 22:31:42 +0300

    Minor fix

--- cs308-server/routes/uploadRoutes.js
+++ cs308-server/routes/uploadRoutes.js
@@ -12,20 +12,21 @@ const performerController = require('../controllers/performerController');
 const songPerformerController = require('../controllers/songPerformerController'); 
 const genreController = require('../controllers/genreController'); 
 const songGenreController = require('../controllers/songGenreController'); 
+const authenticateToken = require('../middleware/authMiddleware');
 
 // Set up the storage for Multer
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      const uploadDir = 'uploads/';
-      // Check if the 'uploads' directory exists, and create it if not
-      if (!fs.existsSync(uploadDir)) {
-        fs.mkdirSync(uploadDir);
-      }
-      cb(null, uploadDir);
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    },
+  destination: function (req, file, cb) {
+    const uploadDir = 'uploads/';
+    // Check if the 'uploads' directory exists, and create it if not
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  },
 });
 
 // Set up the file filter to only accept .csv files
@@ -75,83 +76,85 @@ const parseCSV = (filePath) => {
 
 // Set up a route for file uploading
 router.post('/', upload.single('file'), async (req, res) => {
-    const { userId } = req.body;
-    // Check userId is valid and user with that ID exists
-    if (userController.validateUser(userId) === false) {
-        return res.status(400).send({
-            status: 'error',
-            code: 400,
-            message: 'Invalid userId',
-        });
-    }
+  const { userId } = req.body;
+  // Check userId is valid and user with that ID exists
+  if (userController.validateUser(userId) === false) {
+    return res.status(400).send({
+        status: 'error',
+        code: 400,
+        message: 'Invalid userId',
+    });
+  }
 
-    if (!req.file) {
-        return res.status(400).send({
-            status: 'error',
-            code: 400,
-            message: 'No file uploaded',
-        });
-    }
+  if (!req.file) {
+    return res.status(400).send({
+        status: 'error',
+        code: 400,
+        message: 'No file uploaded',
+    });
+  }
 
-    // Get the path of the uploaded file
-    const filePath = req.file.path;
+  // Get the path of the uploaded file
+  const filePath = req.file.path;
 
-    try {
-        // Parse the CSV file and extract fields
-        const parsedData = await parseCSV(filePath);
+  try {
+    // Parse the CSV file and extract fields
+    const parsedData = await parseCSV(filePath);
 
-        // Further processing with the extracted fields
-        console.log('Extracted Fields:', parsedData);
+    // Further processing with the extracted fields
+    console.log('Extracted Fields:', parsedData);
 
-        // Add each song inside parsedData to the database
-        for (const song of parsedData) {
-            // Check if song already exists in database, if exists link user to song and skip
-            const existingSong = await songController.getSongByTitleAndAlbum(song.title, song.album);
-            if (existingSong) {
-                // Add song to UserSongs table
-                await userSongController.linkUserSong(userId, existingSong.SongID);
-            } else {
-                // Add song to Songs table
-                const songId = await songController.createSong(song);
-                // Add song to UserSongs table
-                await userSongController.linkUserSong(userId, songId.SongID);
+    // Add each song inside parsedData to the database
+    for (const song of parsedData) {
+      // Check if song already exists in database, if exists link user to song and skip
+      const existingSong = await songController.getSongByTitleAndAlbum(song.title, song.album);
+      if (existingSong) {
+        // Add song to UserSongs table
+        await userSongController.linkUserSong(userId, existingSong.SongID);
+      } else {
+        // Add song to Songs table
+        const songId = await songController.createSong(song);
+        // Add song to UserSongs table
+        await userSongController.linkUserSong(userId, songId.SongID);
 
-                // Seperate performers and genres into arrays
-                const performersArr = song.performers.split(',').trim();
-                const genresArr = song.genres.split(',').trim();
+        // Seperate performers and genres into arrays
+        const performersArr = song.performers.split(',').map((performer) => performer.trim());
+        const genresArr = song.genres.split(',').map((genre) => genre.trim());
 
-                // Add performers of the song to Performers table, one by one
-                for (const performer of performersArr) {
-                    console.log('Performer:', performer);
-                    const performerIds = await performerController.createPerformer(performer, null);
-                    // Add song to SongPerformers table
-                    await songPerformerController.linkSongPerformer(songId.SongID, performerIds.PerformerID);
-                }
-                
-                // Add genres of the song to Genres table, one by one
-                for (const genre of genresArr) {
-                    console.log('Genre:', genre);
-                    const genreIds = await genreController.createGenre(genre);
-                    // Add song to SongGenres table
-                    await songGenreController.linkSongGenre(songId.SongID, genreIds.GenreID);
-                }
-            }
+        // Add performers of the song to Performers table, one by one
+        for (const performer of performersArr) {
+          console.log('Performer:', performer);
+          const performerIds = await performerController.createPerformer(performer, null);
+          // Add song to SongPerformers table
+          await songPerformerController.linkSongPerformer(songId.SongID, performerIds.PerformerID);
         }
-
-        res.status(200).send({
-            status: 'success',
-            code: 200,
-            message: 'Song(s) uploaded from CSV file successfully',
-            data: parsedData,
-        });
-    } catch (error) {
-        console.error('Error parsing CSV:', error);
-        res.status(500).send({
-            status: 'error',
-            code: 500,
-            message: 'Internal Server Error',
-        })
+        
+        // Add genres of the song to Genres table, one by one
+        for (const genre of genresArr) {
+          console.log('Genre:', genre);
+          const genreIds = await genreController.createGenre(genre);
+          // Add song to SongGenres table
+          await songGenreController.linkSongGenre(songId.SongID, genreIds.GenreID);
+        }
+        
+        console.log('Song added to database successfully.');
+      }
     }
+
+    res.status(200).send({
+        status: 'success',
+        code: 200,
+        message: 'Song(s) uploaded from CSV file successfully',
+        data: parsedData,
+    });
+  } catch (error) {
+    console.error('Error parsing CSV:', error);
+    res.status(500).send({
+        status: 'error',
+        code: 500,
+        message: 'Internal Server Error',
+    })
+  }
 });
 
 module.exports = router;
